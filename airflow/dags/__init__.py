fdfdeb7770dbe15908ff5abcd5b50cfa1ccd9046
denis-k2 <dxxx999@gmail.com>
2023-12-02 19:56:50 +0700

    ruff: lint & format 'airflow'

--- airflow/dags/__init__.py
+++ airflow/dags/__init__.py
@@ -5,41 +5,38 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 from .operator import CurrencyScoopOperator
 
-
 with DAG(
-        dag_id='exchange_rate',
-        start_date=datetime(2022, 7, 1),
-        schedule_interval='@daily',
-        catchup=False,
+    dag_id="exchange_rate",
+    start_date=datetime(2022, 7, 1),
+    schedule_interval="@daily",
+    catchup=False,
 ) as dag:
-
     create_table = PostgresOperator(
-        task_id='create_table_task',
-        postgres_conn_id='numbeo_conn_id',
-        sql='sql/create_table.sql'
+        task_id="create_table_task",
+        postgres_conn_id="numbeo_conn_id",
+        sql="sql/create_table.sql",
     )
 
     tasks = []
 
-
     get_rate_task = CurrencyScoopOperator(
-        task_id='get_rate',
-        base_currency='USD',
-        #currency=currency,
-        conn_id='cur_scoop_conn_id',
+        task_id="get_rate",
+        base_currency="USD",
+        # currency=currency,
+        conn_id="cur_scoop_conn_id",
         dag=dag,
         do_xcom_push=True,
     )
-    
+
     insert_rate = PostgresOperator(
-        task_id=f'insert_rate',
-        postgres_conn_id='numbeo_conn_id',
-        sql='sql/insert_rate.sql',
+        task_id="insert_rate",
+        postgres_conn_id="numbeo_conn_id",
+        sql="sql/insert_rate.sql",
         params={
-            'base_currency': 'USD',
-            #'currency': currency,
-            'get_rate_task_id': 'get_rate'
-        }
+            "base_currency": "USD",
+            # "currency": currency,
+            "get_rate_task_id": "get_rate",
+        },
     )
 
 create_table >> get_rate_task >> insert_rate
--- airflow/dags/hook.py
+++ airflow/dags/hook.py
@@ -4,29 +4,28 @@ from airflow.hooks.base import BaseHook
 
 
 class CurrencyScoopHook(BaseHook):
-
     def __init__(self, currency_conn_id: str):
         super().__init__()
         self.conn_id = currency_conn_id
 
     def get_rate(self, date, base_currency: str):
-        url = 'https://api.currencyscoop.com/v1/historical'
+        url = "https://api.currencyscoop.com/v1/historical"
         params = {
-            'base': 'USD',
-            #'symbols': currency.upper(),
-            'api_key': self._get_api_key(),
-            'date': str(date),
+            "base": "USD",
+            # "symbols": currency.upper(),
+            "api_key": self._get_api_key(),
+            "date": str(date),
         }
         response = requests.get(url, params=params)
         response.raise_for_status()
-        byn = response.json()['response']['rates']['BYN']
-        eur = response.json()['response']['rates']['EUR']
-        rub = response.json()['response']['rates']['RUB']
-        uah = response.json()['response']['rates']['UAH']
+        byn = response.json()["response"]["rates"]["BYN"]
+        eur = response.json()["response"]["rates"]["EUR"]
+        rub = response.json()["response"]["rates"]["RUB"]
+        uah = response.json()["response"]["rates"]["UAH"]
         return (byn, eur, rub, uah)
 
     def _get_api_key(self):
         conn = self.get_connection(self.conn_id)
         if not conn.password:
-            raise AirflowException('Missing API key (password) in connection settings')
+            raise AirflowException("Missing API key (password) in connection settings")
         return conn.password
