f98b3350405984669eebf11c5e5720254bec3520
denis-k2 <dxxx999@gmail.com>
2023-12-02 21:08:02 +0700

    ruff: minimal lint & format 'scraping'

--- scraping/climate_first_scrap_SELENIUM.py
+++ scraping/climate_first_scrap_SELENIUM.py
@@ -1,22 +1,22 @@
+import logging
+import re
+from datetime import date
 from os import getenv
 from time import time
-from datetime import date
-import re
-import logging
 
 import pandas as pd
-from bs4 import BeautifulSoup
 import psycopg2
+from bs4 import BeautifulSoup
 from psycopg2 import Error
 from selenium import webdriver
 
 
 def construct_url(country, state, city):
-    if country == 'United States':
-        country = ''.join([state.replace(' ', '-'), '-usa'])
+    if country == "United States":
+        country = "".join([state.replace(" ", "-"), "-usa"])
     else:
-        country = country.replace(' ', '-')
-    city = city.replace(' ', '-')
+        country = country.replace(" ", "-")
+    city = city.replace(" ", "-")
     url = f"https://www.weather-atlas.com/en/{country}/{city}-climate?c,mm,mb,km"
     # Units of measurement are forced in the request
     # In the future I will not do reconciliation on them
@@ -28,19 +28,21 @@ def scrap_city_dict(url):
     try:
         # r = requests.get(url, allow_redirects=False).text
         driver.get(url=url)
-        driver.find_element('xpath', '/html/body/div[1]/div[3]/div[2]/div[1]/a[1]').click()
+        driver.find_element(
+            "xpath", "/html/body/div[1]/div[3]/div[2]/div[1]/a[1]"
+        ).click()
         r = driver.page_source
-        soup = BeautifulSoup(r, 'lxml')
-        ul_tags = soup.find_all('ul', class_='list-unstyled mb-0')
+        soup = BeautifulSoup(r, "lxml")
+        ul_tags = soup.find_all("ul", class_="list-unstyled mb-0")
         if not ul_tags:
-            print(f'wrong url: {url}')
+            print(f"wrong url: {url}")
         else:
             for ul in ul_tags:
-                li_tags = ul.find_all('li')
+                li_tags = ul.find_all("li")
                 for li in li_tags:
                     city_dict[li.a.text] = li.span.text
             for key, value in city_dict.items():
-                city_dict[key] = re.split(r'(\-?\d*\.?\d+|\d+)', value)
+                city_dict[key] = re.split(r"(\-?\d*\.?\d+|\d+)", value)
     except Exception as ex:
         print(f"[INFO] {url} Error: ", ex)
     return city_dict
@@ -49,11 +51,22 @@ def scrap_city_dict(url):
 def get_params_dict(city_dict):
     params_dict = {}
     for key, value in city_dict.items():
-        item = key.split(' in ')
+        item = key.split(" in ")
         param = item[0]
         if param not in params_dict.keys():
-            params_dict[param] = [param.removeprefix('Average ').removesuffix('erature').replace(' ', '_').lower()]
-            params_dict[param].append(value[2].strip().lstrip('В').replace('h', 'hours').replace('km/hours', 'km/h'))
+            params_dict[param] = [
+                param.removeprefix("Average ")
+                .removesuffix("erature")
+                .replace(" ", "_")
+                .lower()
+            ]
+            params_dict[param].append(
+                value[2]
+                .strip()
+                .lstrip("В")
+                .replace("h", "hours")
+                .replace("km/hours", "km/h")
+            )
     return params_dict
 
 
@@ -61,7 +74,7 @@ def get_months_dict(city_dict):
     count = 0
     months_dict = {}
     for key, value in city_dict.items():
-        item = key.split(' in ')
+        item = key.split(" in ")
         month = item[1]
         if month not in months_dict.keys():
             count += 1
@@ -70,10 +83,10 @@ def get_months_dict(city_dict):
 
 
 def create_climate_into_db(params_dict):
-    climate_params = ''  # Part to insert into SQL table creation
-    comments = ''  # SQL query to add comments to a table
+    climate_params = ""  # Part to insert into SQL table creation
+    comments = ""  # SQL query to add comments to a table
     for key, value in params_dict.items():
-        climate_params += f'{value[0]} numeric(5,1),'
+        climate_params += f"{value[0]} numeric(5,1),"
         comments += f"COMMENT ON COLUMN avg_climate.{value[0]} IS '{key}, {value[1]}';"
     with open("./sql/create_avg_climate.sql") as sql_script:
         sql_script = sql_script.read().format(climate_params=climate_params)
@@ -91,16 +104,18 @@ def get_columns_list(params_dict):
 
 
 def params_template_df(months_dict, columns_list):
-    df_params_templ = pd.DataFrame(index=months_dict.values(), columns=columns_list, dtype=None)
+    df_params_templ = pd.DataFrame(
+        index=months_dict.values(), columns=columns_list, dtype=None
+    )
     df_params_templ.loc[:, :] = None
-    df_params_templ.insert(0, 'city_id', None)
-    df_params_templ.insert(1, 'month', df_params_templ.index)
+    df_params_templ.insert(0, "city_id", None)
+    df_params_templ.insert(1, "month", df_params_templ.index)
     return df_params_templ
 
 
 def fill_params_template_df(city_dict, months_dict, params_dict, df_params_fill):
     for key, value in city_dict.items():
-        item = key.split(' in ')
+        item = key.split(" in ")
         month = months_dict[item[1]]
         column = params_dict[item[0]][0]
         num = float(value[1])
@@ -109,16 +124,18 @@ def fill_params_template_df(city_dict, months_dict, params_dict, df_params_fill)
     #  = df_params_full
 
 
-if __name__ == '__main__':
-    DATA_ENGR = getenv('DATA_ENGR')
-    URL = getenv('SQLALCHEMY_RELOHELPER_URL')
+if __name__ == "__main__":
+    DATA_ENGR = getenv("DATA_ENGR")
+    URL = getenv("SQLALCHEMY_RELOHELPER_URL")
     # ========================== change *.log ========================== #
-    logging.basicConfig(filename="./data/logs_usa.log", filemode="w", level=logging.INFO)
+    logging.basicConfig(
+        filename="./data/logs_usa.log", filemode="w", level=logging.INFO
+    )
     current_date = date.today()
     # ========================== change *.pkl ========================== #
     df_numbeo = pd.read_pickle("./data/numbeo_links_usa.pkl")
     # Sorting to make it easier to find the error in the url
-    df_numbeo.sort_values('country', inplace=True)
+    df_numbeo.sort_values("country", inplace=True)
     driver = webdriver.Firefox()
     driver.implicitly_wait(10)
 
@@ -127,7 +144,9 @@ if __name__ == '__main__':
         cursor = connection.cursor()
 
         # Create 'avg_climate' table in DB
-        url_instance = 'https://www.weather-atlas.com/en/canada/vancouver-climate?c,mm,mb,km'
+        url_instance = (
+            "https://www.weather-atlas.com/en/canada/vancouver-climate?c,mm,mb,km"
+        )
         city_dict_instance = scrap_city_dict(url_instance)
         params_dict = get_params_dict(city_dict_instance)
         months_dict = get_months_dict(city_dict_instance)
@@ -139,15 +158,20 @@ if __name__ == '__main__':
 
         start_time = time()
         for index, row in df_numbeo.iterrows():
-            url = construct_url(row['country'], row['state_name'], row['city'])
+            url = construct_url(row["country"], row["state_name"], row["city"])
             city_dict = scrap_city_dict(url)
             if not city_dict:
-                logging.info('city_id:%s:wrong_url:%s', index, url)
+                logging.info("city_id:%s:wrong_url:%s", index, url)
             else:
                 df_params_fill = df_params_empty.copy()
-                df_params_fill['city_id'] = index
-                df_params_full = fill_params_template_df(city_dict, months_dict, params_dict, df_params_fill)
-                df_params_full[['sys_updated_date', 'sys_updated_by']] = [date.today(), DATA_ENGR]
+                df_params_fill["city_id"] = index
+                df_params_full = fill_params_template_df(
+                    city_dict, months_dict, params_dict, df_params_fill
+                )
+                df_params_full[["sys_updated_date", "sys_updated_by"]] = [
+                    date.today(),
+                    DATA_ENGR,
+                ]
                 for row in df_params_full.itertuples(index=False):
                     cursor.execute("INSERT INTO avg_climate VALUES %s", (tuple(row),))
                 connection.commit()
@@ -160,4 +184,4 @@ if __name__ == '__main__':
             finish_time = time()
             print("[INFO] Postgres connection closed.")
             print("Code execution time: ", finish_time - start_time)
-            logging.info('Finished scraping: %s', finish_time - start_time)
+            logging.info("Finished scraping: %s", finish_time - start_time)
--- scraping/scraping_numbeo_stats.py
+++ scraping/scraping_numbeo_stats.py
@@ -1,29 +1,30 @@
+from datetime import date, datetime
 from os import getenv
 from time import time
-from datetime import datetime, date
 
 import pandas as pd
+import psycopg2
 import requests
 from bs4 import BeautifulSoup
-import psycopg2
 from psycopg2 import Error
 
-
 skip_list = [
-    'Domestic Non-Alcoholic Beer (0.5 liter draught)',
-    'Domestic Non-Alcoholic Beer (0.5 liter bottle)',
-    'Imported Non-Alcoholic Beer (0.33 liter bottle)',
-    'Bottle of Non-Alcoholic Wine (Mid-Range)',
-    'Buffalo Round (1kg) (or Equivalent Back Leg Red Meat)'
+    "Domestic Non-Alcoholic Beer (0.5 liter draught)",
+    "Domestic Non-Alcoholic Beer (0.5 liter bottle)",
+    "Imported Non-Alcoholic Beer (0.33 liter bottle)",
+    "Bottle of Non-Alcoholic Wine (Mid-Range)",
+    "Buffalo Round (1kg) (or Equivalent Back Leg Red Meat)",
 ]
 
 
 def create_df_summary_empty():
     summary_dict = {
-        'Restaurants': ['Summary',
-                        'Family of four estimated monthly costs (without rent)',
-                        'A single person estimated monthly costs (without rent)'],
-        'Edit': ['Edit', None, None]
+        "Restaurants": [
+            "Summary",
+            "Family of four estimated monthly costs (without rent)",
+            "A single person estimated monthly costs (without rent)",
+        ],
+        "Edit": ["Edit", None, None],
     }
     return pd.DataFrame(summary_dict)
 
@@ -33,39 +34,43 @@ def get_response(link):
 
 
 def get_soup(response):
-    return BeautifulSoup(response, 'lxml')
+    return BeautifulSoup(response, "lxml")
 
 
 def create_df_summary_complete(soup, df_summary_empty):
     try:
-        div = soup.find('div', class_='seeding-call table_color summary limit_size_ad_right padding_lower other_highlight_color')
-        items = div.find_all('li')
-        costs_list = items[0].span.text.split(' ')
-        if costs_list[0][-1] == '$':
+        div = soup.find(
+            "div",
+            class_=("seeding-call table_color summary limit_size_ad_right "
+                    "padding_lower other_highlight_color"),
+        )
+        items = div.find_all("li")
+        costs_list = items[0].span.text.split(" ")
+        if costs_list[0][-1] == "$":
             family_costs = costs_list[0]
-            person_costs = items[1].span.text.split(' ')[0]
-        elif costs_list[1][-2] == '$':
-            family_costs = costs_list[1].strip('()')
-            person_costs = items[1].span.text.split(' ')[1].strip('()')
+            person_costs = items[1].span.text.split(" ")[0]
+        elif costs_list[1][-2] == "$":
+            family_costs = costs_list[1].strip("()")
+            person_costs = items[1].span.text.split(" ")[1].strip("()")
         else:
             family_costs = None
             person_costs = None
     except:
         family_costs = None
         person_costs = None
-    
+
     df_summary_complete = df_summary_empty.copy()
-    df_summary_complete['Edit'][1] = family_costs
-    df_summary_complete['Edit'][2] = person_costs
-    
+    df_summary_complete["Edit"][1] = family_costs
+    df_summary_complete["Edit"][2] = person_costs
+
     return df_summary_complete
 
 
 def last_update_pars(soup):
     try:
-        div = soup.find('div', class_='align_like_price_table')
-        last_date = div.find('br').next.lstrip('Last update: ').rstrip('\n')
-        last_update = datetime.strptime(last_date, '%B %Y').date()
+        div = soup.find("div", class_="align_like_price_table")
+        last_date = div.find("br").next.lstrip("Last update: ").rstrip("\n")
+        last_update = datetime.strptime(last_date, "%B %Y").date()
     except:
         last_update = None
     return last_update
@@ -76,47 +81,70 @@ def create_main_table(responce):
 
 
 def tidy_main_table(table):
-    table[['Edit', 'Range']] = table[['Edit', 'Range']].replace(',', '', regex=True)
+    table[["Edit", "Range"]] = table[["Edit", "Range"]].replace(",", "", regex=True)
     table.Edit = table.Edit.replace({"?": None})
-    table.Edit = table.Edit.str.strip('\xa0$')
-    table.Range = table.Range.str.split('-')
+    table.Edit = table.Edit.str.strip("\xa0$")
+    table.Range = table.Range.str.split("-")
     table = table.where(pd.notnull(table), None)
-    table = table.loc[table.Edit != 'Edit']
-    
+    table = table.loc[table.Edit != "Edit"]
+
     for _, row in table.iterrows():
         try:
             row.Range = str(list(map(float, row.Range)))
         except:
             pass
-        
+
     return table
 
 
 def main_table_into_db(table, index, current_date, data_engr):
-    param_id = iter([5, 26])  # Primary Keys (param_id) from numbeo_param for 'Imported Beer (0.33 liter bottle)'
+    param_id = iter(
+        [5, 26]
+    )  # PrimaryKeys(param_id) from numbeo_param for 'Imported Beer (0.33 liter bottle)'
     for _, row in table.iterrows():
-        if row['Restaurants'] != 'Imported Beer (0.33 liter bottle)' and row['Restaurants'] not in skip_list:
+        if (
+            row["Restaurants"] != "Imported Beer (0.33 liter bottle)"
+            and row["Restaurants"] not in skip_list
+        ):
             cursor.execute(
-                "INSERT INTO numbeo_stat (city_id, param_id, cost, range, updated_date, sys_updated_date, sys_updated_by) \
+                "INSERT INTO numbeo_stat (city_id, param_id, cost, range, \
+                updated_date, sys_updated_date, sys_updated_by) \
                 VALUES (%s, \
                 (SELECT param_id FROM numbeo_param WHERE param = %s), \
                 %s, %s, %s, %s, %s)",
-                (index, row['Restaurants'], row['Edit'], row['Range'], last_update, current_date, data_engr)
+                (
+                    index,
+                    row["Restaurants"],
+                    row["Edit"],
+                    row["Range"],
+                    last_update,
+                    current_date,
+                    data_engr,
+                ),
             )
         elif row.Restaurants in skip_list:
             pass
         else:
             cursor.execute(
-                "INSERT INTO numbeo_stat (city_id, param_id, cost, range, updated_date, sys_updated_date, sys_updated_by) \
+                "INSERT INTO numbeo_stat (city_id, param_id, cost, range, \
+                updated_date, sys_updated_date, sys_updated_by) \
                 VALUES (%s, %s, %s, %s, %s, %s, %s)",
-                (index, next(param_id), row['Edit'], row['Range'], last_update, current_date, data_engr)
+                (
+                    index,
+                    next(param_id),
+                    row["Edit"],
+                    row["Range"],
+                    last_update,
+                    current_date,
+                    data_engr,
+                ),
             )
         connection.commit()
 
 
-if __name__ == '__main__':
-    DATA_ENGR = getenv('DATA_ENGR')
-    URL = getenv('SQLALCHEMY_RELOHELPER_URL')
+if __name__ == "__main__":
+    DATA_ENGR = getenv("DATA_ENGR")
+    URL = getenv("SQLALCHEMY_RELOHELPER_URL")
     current_date = date.today()
     # ========================== change *.pkl ========================== #
     df = pd.read_pickle("./data/numbeo_links.pkl")
@@ -126,14 +154,16 @@ if __name__ == '__main__':
     try:
         connection = psycopg2.connect(URL)
         cursor = connection.cursor()
-    
+
         for index, row in df.iterrows():
-            link = row['link']
+            link = row["link"]
             response = get_response(link)
             soup = get_soup(response)
             df_summary_complete = create_df_summary_complete(soup, df_summary_empty)
             df_main_table = create_main_table(response)
-            df_main_table = pd.concat([df_main_table, df_summary_complete], ignore_index=True)
+            df_main_table = pd.concat(
+                [df_main_table, df_summary_complete], ignore_index=True
+            )
             df_main_table = tidy_main_table(df_main_table)
             last_update = last_update_pars(soup)
             main_table_into_db(df_main_table, index, current_date, DATA_ENGR)
