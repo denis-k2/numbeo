b003aaa9185205b343bc09be3a1dcc1d388fde7f
denis-k2 <dxxx999@gmail.com>
2023-12-29 18:58:21 +0700

    fix: coordinate the table with SQLAlchemy

--- airflow/dags/sql/create_table.sql
+++ airflow/dags/sql/create_table.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS exchange_rates (
+CREATE TABLE IF NOT EXISTS exchange_rate (
 	currency varchar(3) NOT NULL DEFAULT 'USD'::character varying,
 	byn numeric(8, 3) NOT NULL,
     eur numeric(8, 3) NOT NULL,
 	rub numeric(8, 3) NOT NULL,
 	uah numeric(8, 3) NOT NULL,
-	responce_date date NOT NULL
-)
+	responce_date date NOT NULL,
+    CONSTRAINT PK_responce_date PRIMARY KEY ( responce_date )
+);
