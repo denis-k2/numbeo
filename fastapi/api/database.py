4cf77f62ef5eaa4c325f30efa352c130032b8327
Denis Smagin <78604607+denis-k2@users.noreply.github.com>
2024-01-25 20:25:38 +0700

    async code for the api part (#17)
    * async code for the api part
    
    * ruff: lint & format

--- fastapi/api/database.py
+++ fastapi/api/database.py
@@ -1,14 +1,18 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from config import settings
 
-SQLALCHEMY_RELOHELPER_URL = settings.relohelper_url
+RELOHELPER_URL = settings.relohelper_url
+RELOHELPER_ASYNC_URL = settings.relohelper_async_url
 
-engine = create_engine(SQLALCHEMY_RELOHELPER_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(RELOHELPER_URL)
+async_engine = create_async_engine(RELOHELPER_ASYNC_URL)
+async_session = async_sessionmaker(
+    async_engine, expire_on_commit=False, class_=AsyncSession
+)
 
 
-def get_db():
-    with SessionLocal() as session:
+async def get_db():
+    async with async_session() as session:
         yield session
